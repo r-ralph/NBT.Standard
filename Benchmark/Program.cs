034e47f8ee34cc46049178597657e0c1ad713a90
Ralph <git@ralph.ms>
2017-05-29 01:34:27 +0900

    Add benchmark project

---
+++ Benchmark/Program.cs
@@ -0,0 +1,16 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Benchmark
+{
+    public class Program
+    {
+        #region Static Methods
+
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<TagWriterBenchmark>();
+        }
+
+        #endregion
+    }
+}
